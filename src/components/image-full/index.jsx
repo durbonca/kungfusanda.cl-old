fa03a138bb92871c1343e2487b99d1607da82c54
durbonca <durbonca@gmail.com>
2022-04-24 17:07:26 -0400

    horario finish

--- src/components/image-full/index.jsx
+++ src/components/image-full/index.jsx
@@ -1,12 +1,25 @@
+import styled from 'styled-components';
+
+const StyledImageFull = styled.div`
+  max-width: '100vw';
+  padding: '10px 3em';
+  @media (max-width: 768px) {
+    padding: '0';
+  }
+`;
+
+const StyledImg = styled.img`
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 20px 8px 0px;
+  border-radius: 15px;
+  @media (max-width: 768px) {
+    border-radius: 0;
+  }
+`;
+
 export const ImageFull = ({ img, altImg }) => {
   return (
-    <div style={{ maxWidth: '100vw', padding: '10px 3em' }}>
-      <img
-        style={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 20px 8px 0px', borderRadius: '15px' }}
-        width="100%"
-        src={img}
-        alt={altImg}
-      />
-    </div>
+    <StyledImageFull>
+      <StyledImg width="100%" src={img} alt={altImg} />
+    </StyledImageFull>
   );
 };
--- src/pages/Horarios.jsx
+++ src/pages/Horarios.jsx
@@ -2,20 +2,22 @@ import Horario1 from '../img/Horario1.jpeg';
 import Horario2 from '../img/Horario2.jpeg';
 
 import { ImageFull } from '../components/';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #fafafa;
+  flex-direction: column;
+`;
 
 export const Horarios = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        minHeight: '100vh',
-        backgroundColor: '#fafafa',
-      }}
-    >
+    <StyledContainer>
       <ImageFull img={Horario1} altImg="horario de clases kung-fu" />
       <ImageFull img={Horario2} altImg="horario de clases sanda" />
-    </div>
+    </StyledContainer>
   );
 };
